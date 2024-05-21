5e0b7eab04bbe93419dd45dcfc9788856dcef809
Erik Faye Karla <erik.karla@cyncly.com>
2024-05-21 14:45:28 +0200

    fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'CYNCLYSECAPP\static\img\CYNCLY_logo_CW_RGB.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://EFKCyncly.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'EFKCyncly', // Usually your GitHub org/user name.
+  projectName: 'SecurityChampions', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
